387df4c1a9b6b78f6a402525e15a031747025877
Kaicobad <kaikobad.kaikobad326@gmail.com>
2024-03-06 16:21:44 +0600

    Second

---
+++ src/Services/Product.API/Context/ProductDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Product.API.Context
+{
+    public class ProductDbContext :DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) :base(options) 
+        {
+            
+        }
+
+        public virtual void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

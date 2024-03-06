d0df47120d1457d548ec5448109b588d309c751a
Kaicobad Hassan <kaikobad.kaikobad326@gmail.com>
2024-03-07 00:24:06 +0600

    2nd

---
+++ src/Services/Product.API/Model/Productcs.cs
@@ -0,0 +1,14 @@
+﻿namespace Product.API.Model
+{
+    public class Productcs
+    {
+        public int ProductId { get; set; }
+        public int ProductCategoryId { get; set; }
+        public int BrandId { get; set; }
+        public int SpareTypeId { get; set; }
+        public int ProductManufacturingPartId { get; set; }
+        public int ProductSpecificationId { get; set; }
+        public int ProductTechnologyId { get; set; }
+
+    }
+}

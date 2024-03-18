39959e956a4717776eb53843247abd2d264e69eb
Kaicobad <kaikobad.kaikobad326@gmail.com>
2024-03-18 11:54:37 +0600

    Started Dapper

---
+++ src/Helper.API/DataContext/HelperDapperContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Helper.API.DataContext
+{
+    public class HelperDapperContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public HelperDapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("HelperConStr");
+        }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
--- src/Helper.API/Program.cs
+++ src/Helper.API/Program.cs
@@ -14,7 +14,7 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("HelperConStr")))
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSingleton<HelperDapperContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
